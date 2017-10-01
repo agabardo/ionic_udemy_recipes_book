31dcb31cde93455a78f53477cc02f2be233ca58a
Ademir C. Gabardo <ademir.gabbardo@gmail.com>
2017-10-01 14:38:53 +1100

    The service is up and running

--- src/pages/shopping-list/shopping-list.ts
+++ src/pages/shopping-list/shopping-list.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { NgForm} from "@angular/forms";
-
+import { ShoppingListService } from "../../services/shopping-list";
 @Component({
   selector: 'page-shopping-list',
   templateUrl: 'shopping-list.html',
 })
 export class ShoppingListPage {
 
+  constructor(private slService:ShoppingListService){
+
+  }
+
   onAddItem(form:NgForm){
-    console.log(form);
+    this.slService.addItem(form.value.ingridientName,form.value.ingridientAmount);
+    form.reset();
   }
 
 }
